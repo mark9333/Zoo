d0324958af459420692314dfcb52d74f3e44a502
Mark Borisov <markmborisov@tu-sofia.bg>
2022-08-01 14:33:06 +0300

    Add project files.

---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Zoo
+{
+
+    public class User
+    {
+        public class User
+        {
+            public int id { get; set; }
+            public string first_name { get; set; }
+            public string last_name { get; set; }
+            public string password { get; set; }
+            public string username { get; set; }
+
+            public User()
+            {
+
+            }
+        }
+    }
+}
---
+++ UserDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    internal class UserDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+
+
+        public UserDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+
+            options.UseMySQL("Server=localhost;uid=root;pwd=123456789aA;Database=zoo");
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<User> Users { get; set; }
+
+    }
+}
